256073ae515d23b4957be9e7f048adcaeab08da0
Modzel200 <49248797+Modzel200@users.noreply.github.com>
2023-06-03 14:03:45 +0200

    subject

---
+++ USOS/USOS/Entities/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using USOS.Enums;
+
+namespace USOS.Entities
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDesc { get; set; }
+        public int Semester { get; set; }
+        public Major MajorSubject { get; set; }
+    }
+}
