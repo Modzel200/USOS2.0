ba46ac5af512d82e7ea14318af33b24bd32166ef
Modzel200 <49248797+Modzel200@users.noreply.github.com>
2023-06-03 14:40:45 +0200

    dependency

--- USOS/USOS/Controllers/StudentController.cs
+++ USOS/USOS/Controllers/StudentController.cs
@@ -1,6 +1,24 @@
-﻿namespace USOS.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using USOS.Entities;
+using USOS.Services;
+
+namespace USOS.Controllers
 {
-    public class StudentController
+    [Route("api/student")]
+    [ApiController]
+    public class StudentController:ControllerBase
     {
+        private StudentService _studentService;
+
+        public StudentController(IStudentService studentService) 
+        { 
+            _studentService = studentService;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAll()
+        {
+            var student = _studentService.GetAll();
+            return Ok(student);
+        }
     }
 }
--- USOS/USOS/Program.cs
+++ USOS/USOS/Program.cs
@@ -1,4 +1,5 @@
 using USOS.Entities;
+using USOS.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<UsosDbContext>();
+builder.Services.AddScoped<IStudentService, StudentService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
