74ef244233fabd42a64d15d0a7d2e86c70caaa40
c0ndii <105825543+c0ndii@users.noreply.github.com>
2023-06-04 16:01:54 +0200

    Migrcja

--- USOS/USOS/Entities/MajorSubject.cs
+++ USOS/USOS/Entities/MajorSubject.cs
@@ -1,12 +1,17 @@
-﻿using USOS.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using USOS.Enums;
 
 namespace USOS.Entities
 {
     public class MajorSubject
     {
+        [Key]
         public int Id { get; set; }
-        public Major Name { get; set; }
-        public string ShortDesc { get; set; }
-        public List<Subject> Subjects { get; set; }
+        [Required(ErrorMessage = "Name is required"), MaxLength(20, ErrorMessage = "Max length is 20 characters"), RegularExpression(@"^[a-zA-Z]+", ErrorMessage = "Only characters are allowed"), DataType(DataType.Text, ErrorMessage = "Only characters are allowed")]
+        public string Name { get; set; }
+        [MaxLength(50, ErrorMessage = "Max length is 50 characters"), RegularExpression(@"^[a-zA-Z]+", ErrorMessage = "Only characters are allowed"), DataType(DataType.Text, ErrorMessage = "Only characters are allowed")]
+        public string? ShortDesc { get; set; }
+        [Required(ErrorMessage = "Subjects are required")]
+        public virtual List<Subject> Subjects { get; set; }
     }
 }
---
+++ USOS/USOS/Migrations/20230604135322_Subjects.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace USOS.Migrations
+{
+    /// <inheritdoc />
+    public partial class Subjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MajorSubjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", nullable: false),
+                    ShortDesc = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MajorSubjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ShortDesc = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Semester = table.Column<int>(type: "int", nullable: false),
+                    MajorSubjectId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subjects_MajorSubjects_MajorSubjectId",
+                        column: x => x.MajorSubjectId,
+                        principalTable: "MajorSubjects",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_MajorSubjectId",
+                table: "Subjects",
+                column: "MajorSubjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "MajorSubjects");
+        }
+    }
+}
